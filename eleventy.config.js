60ddee4eda7ee503c220b0ce4ff7258548eac854
David Matthew <davidmatthew.ie@gmail.com>
2025-04-05 21:32:26 +0100

    Configured terser and created minify function.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,16 @@
-export default async function(eleventyConfig) {
-	return {
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { minify } from "terser";
+
+export default async function (eleventyConfig) {
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addFilter("jsmin", async function (code) {
+    const minified = await minify(code);
+    return minified.code;
+  });
+
+  return {
     dir: {
       input: 'content',
       output: 'dist',
